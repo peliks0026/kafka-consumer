0653e6700c3ede530cb35b4e5de24fd99c43dc87
John Felix Ilagan <ilaganfelix@gmail.com>
2020-04-20 01:23:04 +0800

    add kafka consumer source

---
+++ src/main/java/com/tutorial/kafka/service/ConsumerService.java
@@ -0,0 +1,16 @@
+package com.tutorial.kafka.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class ConsumerService {
+
+    @KafkaListener(topics = "${listener.messages-topic}")
+    public void consume(String message) {
+        log.info(String.format("Consuming message -> %s", message));
+    }
+
+}
